3fc3f5505fb2d5216fae6ce6ab9ad63192ecde26
sheroz34 <sheroz@copypanthers.com>
2017-03-14 16:56:50 +0300

    WA-1482 Media library - working on add/update

--- includes/class-wordapp-seo.php
+++ includes/class-wordapp-seo.php
@@ -222,6 +222,43 @@ function ajax_wordapp_seo() {
                 }
                 $success = true;
             }
+            else if ($cmd=='add-media')
+            {
+                $params = $json['data'];
+
+                $jsonFile = $params['file'];
+                $alt = $params['alt'];
+                $caption = $params['caption'];
+
+                $filename = $jsonFile['name'];
+                $bits = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $jsonFile['body']));
+
+                $parent_post_id = null;
+
+                $upload_file = wp_upload_bits($filename, null, $bits);
+                if (!$upload_file['error']) {
+
+                    $wp_filetype = wp_check_filetype($filename, null );
+                    $attachment = array(
+                        'post_mime_type' => $wp_filetype['type'],
+                        'post_parent' => $parent_post_id,
+                        'post_title' => preg_replace('/\.[^.]+$/', '', $filename),
+                        'post_content' => '',
+                        'post_status' => 'inherit'
+                    );
+
+                    $attachment_id = wp_insert_attachment( $attachment, $upload_file['file'], $parent_post_id );
+                    if (!is_wp_error($attachment_id)) {
+//                        require_once(ABSPATH . "wp-admin" . '/includes/image.php');
+                        $attachment_data = wp_generate_attachment_metadata( $attachment_id, $upload_file['file'] );
+                        wp_update_attachment_metadata( $attachment_id,  $attachment_data );
+                        $success = true;
+                        $data = '';
+                    } else
+                        $data = 'File upload error! wp_insert_attachment()';
+                } else
+                    $data = 'File upload error! wp_upload_bits()';
+            }
             else
                 $data = 'No valid command';
         }
