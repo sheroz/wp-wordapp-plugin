a9ed087f697059e86d929d5fb1472a67474862e7
sheroz34 <sheroz@copypanthers.com>
2017-04-04 20:22:48 +0300

    WA-1482 WA_API http headers to pass REST API validation

--- includes/wa-api-pdx.php
+++ includes/wa-api-pdx.php
@@ -128,7 +128,14 @@ function ajax_wa_pdx() {
 
                 $api_pdx_tickets_url = $api_pdx_url . '/tickets';
                 $args = array('ticket_id' => $ticket_id);
-
+/*
+accept: application/json
+accept-encoding: gzip, deflate
+accept-language: en-US,en;q=0.8
+content-type: application/json
+user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36
+X-Api-Version: application/vnd.wordapp-v1+json
+*/
                 $response = wp_remote_post($api_pdx_tickets_url, $args);
 
                 if ( is_wp_error( $response ) ) {
