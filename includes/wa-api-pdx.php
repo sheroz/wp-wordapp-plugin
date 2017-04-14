ef545c7298b8d0fbe29aaa710c9610fb1ca3ae74
sheroz34 <sheroz@copypanthers.com>
2017-04-14 11:39:42 +0300

    WA-1768 Added browser cache invalidation fix for review urls

--- includes/wa-api-pdx.php
+++ includes/wa-api-pdx.php
@@ -7,8 +7,62 @@
 
 require_once 'wa-api-pdx-const.php';
 
-function wa_pdx_filter_pre_get_posts( $query ) {
+function wa_pdx_generate_preview_access_token ($post_id, $preview_token)
+{
+    // pat means: Preview Access Token
+    // pat should be random for every function call to avoid browser cache
+    // and must be verifiable by preview_token
 
+    // !!! temporary solution. Needs to improve for security reasons
+    // !!! ex. we can  pat = ($salt XOR $preview_token) + $salt
+    // !!! OR use hash with post related stuff to prevent predictions
+    $salt = wa_pdx_random_hex_string(16);
+    $wa_pat = $preview_token . $salt;
+
+    return $wa_pat;
+}
+
+function wa_pdx_check_preview_access_token ($post_id, $wa_pat, $preview_token)
+{
+    return substr($wa_pat, 0, strlen($preview_token)) === $preview_token;
+}
+
+function wa_pdx_add_url_params ($url, $params)
+{
+    if (empty($params))
+        return $url;
+
+    $query = '?';
+    if (strpos($url, $query) !== false)
+        $query = '&';
+
+    if (is_array($params))
+    {
+        $prefix = '';
+        foreach ($params as $param)
+        {
+            $query .= $prefix . $param;
+            $prefix = '&';
+        }
+    }
+    else
+        $query .= $params;
+
+    return $url . $query;
+}
+
+function wa_pdx_random_hex_string($len)
+{
+    $chars = '0123456789abcdef';
+    $chars_len = strlen($chars);
+    $res = '';
+    for ($pos = 0; $pos < $len; $pos++)
+        $res .= $chars[rand(0, $chars_len - 1)];
+    return $res;
+}
+
+function wa_pdx_filter_pre_get_posts( $query )
+{
     if (PDX_LOG_ENABLE)
     {
         $log  = "wa_pdx_filter_pre_get_posts(): Phase 1 passed\n";
@@ -18,19 +72,13 @@ function wa_pdx_filter_pre_get_posts( $query ) {
         file_put_contents(PDX_LOG_FILE, $log, FILE_APPEND);
     }
 
-    if (
-        $query->is_main_query() &&
-        $query->is_preview() &&
-        $query->is_singular()
-    ) {
+    if ($query->is_main_query() && $query->is_preview() && $query->is_singular()) {
         add_filter( 'posts_results', 'wa_pdx_filter_posts_results', 10, 2 );
-
         if (PDX_LOG_ENABLE)
         {
             $log  = "wa_pdx_filter_pre_get_posts(): Phase 2 passed\n";
             file_put_contents(PDX_LOG_FILE, $log, FILE_APPEND);
         }
-
     }
 
     if (PDX_LOG_ENABLE)
@@ -42,8 +90,8 @@ function wa_pdx_filter_pre_get_posts( $query ) {
     return $query;
 }
 
-function wa_pdx_filter_posts_results( $posts ) {
-
+function wa_pdx_filter_posts_results( $posts )
+{
     if (PDX_LOG_ENABLE)
     {
         $log  = "wa_pdx_filter_posts_results(): Phase 1 passed\n";
@@ -58,12 +106,12 @@ function wa_pdx_filter_posts_results( $posts ) {
     if (sizeof($posts) != 1)
         return $posts;
 
-    $wa_preview_token = $_GET['wa_token'];
-    if (empty($wa_preview_token))
+    $wa_pat = $_GET['wa_pat'];
+    if (empty($wa_pat))
     {
         if (PDX_LOG_ENABLE)
         {
-            $log = "wa_token parameter is empty\n";
+            $log = "Invalid wa_pat parameter\n";
             file_put_contents(PDX_LOG_FILE, $log, FILE_APPEND);
         }
         return $posts;
@@ -80,22 +128,23 @@ function wa_pdx_filter_posts_results( $posts ) {
         return $posts;
     }
 
-    $cfg_preview_token = $cfg['preview_token'];
-    if (empty($cfg_preview_token))
+    $preview_token = $cfg['preview_token'];
+    if (empty($preview_token))
     {
         if (PDX_LOG_ENABLE)
         {
-            $log  = "wa_pdx_filter_posts_results(): Invalid configuration. Empty preview_token\n";
+            $log  = "wa_pdx_filter_posts_results(): Invalid configuration\n";
             file_put_contents(PDX_LOG_FILE, $log, FILE_APPEND);
         }
         return $posts;
     }
 
-    if ($cfg_preview_token!=$wa_preview_token)
+    $post_id = $posts[0]->ID;
+    if (!wa_pdx_check_preview_access_token ($post_id, $wa_pat, $preview_token))
     {
         if (PDX_LOG_ENABLE)
         {
-            $log  = "wa_pdx_filter_posts_results(): Not authorized. Tokens are different\n";
+            $log  = "wa_pdx_filter_posts_results(): Not authorized\n";
             file_put_contents(PDX_LOG_FILE, $log, FILE_APPEND);
         }
         return $posts;
@@ -104,8 +153,8 @@ function wa_pdx_filter_posts_results( $posts ) {
     $posts[0]->post_status = 'publish';
 
     // Disable comments and pings for this post.
-    add_filter( 'comments_open', '__return_false' );
-    add_filter( 'pings_open', '__return_false' );
+    add_filter('comments_open', '__return_false');
+    add_filter('pings_open', '__return_false');
 
     if (PDX_LOG_ENABLE)
     {
@@ -522,26 +571,21 @@ function wa_pdx_get_posts()
 
 function wa_pdx_op_content_get_list ()
 {
+    $cfg = get_option(PDX_CONFIG_OPTION_KEY);
+    if (empty($cfg))
+        wa_pdx_send_response('Invalid Configuration');
+
+    $preview_token = $cfg['preview_token'];
     $posts = wa_pdx_get_posts();
     $count = count($posts);
-    $cfg = get_option(PDX_CONFIG_OPTION_KEY);
-
-    $preview_token = '';
-    if (!empty($cfg))
-        $preview_token = $cfg['preview_token'];
-
     for ($pos = 0; $pos < $count; $pos++)
     {
+        $wa_pat = wa_pdx_generate_preview_access_token ($posts[$pos]['ID'], $preview_token);
         if ($posts[$pos]['status'] != 'publish')
-            $posts[$pos]['preview_url'] = $posts[$pos]['url'] . "&preview=true&wa_token=$preview_token";
+            $posts[$pos]['preview_url'] = wa_pdx_add_url_params ($posts[$pos]['url'], "preview=true&wa_pat=$wa_pat");
         else
-            $posts[$pos]['preview_url'] = $posts[$pos]['url'];
+            $posts[$pos]['preview_url'] = wa_pdx_add_url_params ($posts[$pos]['url'], "wa_pat=$wa_pat");
     }
-
-    // foreach ( $posts as $post ) {
-    //    $post['preview_url'] = $post['url'] . "&wa_preview = '1'";
-    // }
-
     wa_pdx_send_response($posts, true);
 }
 
@@ -718,26 +762,31 @@ function wa_pdx_op_media_get_list ()
     wa_pdx_send_response($data, true);
 }
 
+function wa_pdx_generate_preview_url ($post_id)
+{
+    $params = array();
+    $post_status = get_post_status($post_id);
+    if ($post_status !== 'publish')
+        $params[] = 'preview=true';
+
+    $cfg = get_option(PDX_CONFIG_OPTION_KEY);
+    if (empty($cfg))
+        wa_pdx_send_response('Invalid Configuration');
+
+    $preview_token = $cfg['preview_token'];
+    $wa_pat = wa_pdx_generate_preview_access_token ($post_id, $preview_token);
+    $params[] = "wa_pat=$wa_pat";
+    $post_url = get_permalink($post_id);
+    return wa_pdx_add_url_params($post_url , $params);
+}
+
 function wa_pdx_op_prepare_preview ($params)
 {
     $preview_url = null;
     $post_id = wa_pdx_content_update ($params);
-    if (!empty($post_id))
-    {
-        $cfg = get_option(PDX_CONFIG_OPTION_KEY);
-        $preview_token = '';
-        if (!empty($cfg))
-            $preview_token = $cfg['preview_token'];
-
-        $post_url = get_permalink($post_id);
-        $preview_url = $post_url;
-
-        $post_status = get_post_status($post_id);
-        if ($post_status != 'publish')
-            $preview_url = $post_url . "&preview=true&wa_token=$preview_token";
-
-        wa_pdx_send_response($preview_url, true);
-    }
-    else
+    if (empty($post_id))
         wa_pdx_send_response('Invalid Post ID');
+
+     $preview_url = wa_pdx_generate_preview_url ($post_id);
+     wa_pdx_send_response($preview_url, true);
 }
