cf26501e0a107eac82c8da34810b15bdef6aab56
Sheroz <sheroz@mail.ru>
2019-02-27 13:39:30 +0300

    Enabling admin access by validating URL ticket params

--- includes/access.php
+++ includes/access.php
@@ -19,13 +19,27 @@
  *              user_id - the user Id to be logged as
  *              post_id - the post Id to be redirected to after successful login
  *                        post_id = -1 for access to admin panel
+ *              ticket  - the ticket for admin access
+ *              expire  - the ticket expire time, in seconds
  * @return mixed JSON that indicates success/failure status
  *               of the operation in 'success' field,
  *               and an appropriate 'data' or 'error' fields.
  */
 function wa_pdx_op_admin_access_url($params)
 {
+    $user_id   = $params['user_id'];
+    $post_id   = $params['post_id'];
+    $ticket    = $params['ticket'];
+    $expire    = $params['expire'];
+    $access_tickets = get_option('wa_pdx_access_tickets');
+    if( empty($access_tickets) )
+        $access_tickets = array();
 
+    $access_tickets[$ticket] = array(
+        'user_id'   => $user_id,
+        'post_id'   => $post_id,
+    );
+    update_option('wa_pdx_access_tickets', $access_tickets);
 }
 
 /**
@@ -35,13 +49,13 @@ function wa_pdx_check_admin_access()
 {
     if($_SERVER['REQUEST_METHOD'] === 'GET' && isset( $_GET['wa_pdx_access_ticket'] ) )
     {
-        $access_ticket = sanitize_key($_GET['wa_pdx_access_ticket']);
-        if( !empty($access_ticket) && $access_ticket.length() == 24)
+        $ticket = sanitize_key($_GET['wa_pdx_access_ticket']);
+        if( strlen($ticket) == 32)
         {
             $access_tickets = get_option('wa_pdx_access_tickets');
-            if( !empty($access_tickets))
+            if( $access_tickets )
             {
-                $access_params = $access_tickets[$access_ticket];
+                $access_params = $access_tickets[$ticket];
                 if( !empty($access_params))
                 {
                     $user_id = $access_params['user_id'];
